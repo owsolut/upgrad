30de08d154484a045ac8566f32f6d8fac1a8e92e
amit <amit@openwriteup.com>
2025-02-15 16:02:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return jsonify({"message": "Backend is running successfully!"})
+    return jsonify({'message': 'Backend is running successfully!'})
 @app.route('/health')
 def health():
-    return jsonify({"status": "healthy"}), 200
-if __name__ == '__main__'
-   app.run(host='0.0.0.0', port=8000)
+    return jsonify({'status': 'healthy'}), 200
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
